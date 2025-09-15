e41c2674aea349cfe9dcecf265e4935b77900bb3
arkdot <198597192+arkdot@users.noreply.github.com>
2025-09-15 10:21:38 +0200

    install: remove current install if exists

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 
 # Links to $HOME/.local/bin
 echo "tmux-sessionizer -> $HOME/.local/bin/tmux-sessionizer"
+
+# Remove old link if it exists
+rm -f $HOME/.local/bin/tmux-sessionizer
+
+# Create new link
 ln -s $(pwd)/tmux-sessionizer $HOME/.local/bin/tmux-sessionizer
